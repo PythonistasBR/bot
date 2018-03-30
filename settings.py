bcea7743921b9f044f5763fa5bd2f6986c415a15
Fabio Cerqueira <fabio@cerqueira.me>
2018-03-30 18:23:00 +0100

    Added configuration for LOG_LEVEL

--- settings.py
+++ settings.py
@@ -3,9 +3,11 @@ import logging
 
 
 API_TOKEN = os.environ.get("TELEGRAM_API_TOKEN", "")
+LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO")
 
 logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=getattr(logging, LOG_LEVEL),
 )
 
 # Configure the enabled apps

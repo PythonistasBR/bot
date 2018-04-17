fcbb9cd18bed2703a6af3361c4b80f7c918f8318
Fabio Cerqueira <fabio@cerqueira.me>
2018-04-17 19:09:13 +0100

    update settings

--- autonomia/settings.py
+++ autonomia/settings.py
@@ -1,20 +1,20 @@
 import logging
 import os
 
+# application configuration
 DEBUG = os.environ.get("DEBUG", "False") == "True"
-API_TOKEN = os.environ.get("TELEGRAM_API_TOKEN", "")
-FIXER_IO_API_TOKEN = os.environ.get("FIXER_IO_API_TOKEN", "")
-MEETUP_API_KEY = os.environ.get("MEETUP_API_KEY", "")
 LOG_LEVEL = os.environ.get("LOG_LEVEL", "INFO")
-
-WEBHOOK_DOMAIN = os.environ.get("WEBHOOK_DOMAIN", "localhost:5000")
-WEBHOOK_PATH = os.environ.get("WEBHOOK_PATH", "hook")
-WEBHOOK_SET_PATH = os.environ.get("WEBHOOK_PATH", "set_webhook")
-
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=getattr(logging, LOG_LEVEL),
 )
+WEBHOOK_DOMAIN = os.environ.get("WEBHOOK_DOMAIN", "localhost:5000")
+WEBHOOK_PATH = os.environ.get("WEBHOOK_PATH", "hook")
+
+# Third party configuration
+API_TOKEN = os.environ.get("TELEGRAM_API_TOKEN", "")
+FIXER_IO_API_TOKEN = os.environ.get("FIXER_IO_API_TOKEN", "")
+MEETUP_API_KEY = os.environ.get("MEETUP_API_KEY", "")
 
 # Configure the enabled apps
 APPS = [

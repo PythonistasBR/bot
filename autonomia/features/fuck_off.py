fbd93fbd52c5d5cf8b0c290cbaa89676789cd5ac
Rondineli <raraujo04@yahoo.com.br>
2018-04-25 09:56:44 +0100

    small change

--- autonomia/features/fuck_off.py
+++ autonomia/features/fuck_off.py
@@ -10,8 +10,12 @@ def cmd_faas(bot, update, args):
         update.message.reply_text("Use: /fuck <who> - To send someone fuck off")
         return
 
+    if "@" in args[0]:
+        to = args[0]
+        update.message.reply_text(to)
+    text = "%20".join(args)
     user = update.message.from_user.first_name
-    message = f"{FAAS_BASE_URL}/{args[0]}/{user}"
+    message = f"{FAAS_BASE_URL}/{text}/{user}"
     update.message.reply_text(message)
 
 

3d1959ab0f441af7b1147fbf0d8e43e2eaf4fb3a
Fabio Cerqueira <fabio@cerqueira.me>
2020-03-24 13:19:49 +0000

    Add sentry configuration

--- autonomia/app_factory.py
+++ autonomia/app_factory.py
@@ -1,7 +1,9 @@
 import os
 
+import sentry_sdk
 from flask import Flask
 from flask_redis import FlaskRedis
+from sentry_sdk.integrations.flask import FlaskIntegration
 
 from autonomia.blueprints.github import github
 from autonomia.telegram_flask import telegram_flask as bot
@@ -14,6 +16,10 @@ def create_app():
     config_file = os.environ.get("SETTINGS_FILE", "settings.py")
     app.config.from_pyfile(config_file)
     # loading apps
+    sentry_dsn = os.environ.get("SENTRY_DSN")
+    if sentry_dsn:
+        sentry_sdk.init(sentry_dsn, integrations=[FlaskIntegration()])
+
     if os.environ.get("REDIS_URL"):
         redis_store.init_app(app)
     bot.init_app(app)

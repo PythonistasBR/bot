5d3c6dd427933c70c1c211559034697ab4e03048
Rondineli <rondineli.gomes.araujo@gmail.com>
2020-08-19 09:50:31 +0100

    WIP - missing unittests

--- autonomia/features/sextou.py
+++ autonomia/features/sextou.py
@@ -1,22 +1,63 @@
 from telegram.ext import CommandHandler
-
 from autonomia.core import bot_handler
+from dateutil import relativedelta
 
-FAAS_BASE_URL = "http://foaas.com/off"
+import urllib.parse
+import datetime
+
+BASE_URL = "https://www.timeanddate.com/countdown/weekend"
+
+MESSAGES = {
+    'monday': 'Ta longe ainda!',
+    'tuesday': 'Ta mais perto, mas ainda eh antevespera da vespera da sexta',
+    'wednesday': 'Antevespera ta ai',
+    'thursday': 'A vespera de sexta, Ja veo o final de semana',
+    'friday': 'Ja sinto o cheiro do sextou!',
+    'saturday': 'Nao enche, aproveita o fds',
+    'sunday': 'Alegria de pobre dura pouco!'
+}
 
 
 def cmd_countdown(bot, update, args):
-    if not args:
-        update.message.reply_text("Use: /sextou? - To see when you will be happy")
+    try:
+        if "@" in args[0]:
+            to = args[0]
+            update.message.reply_text(to)
+
+    except IndexError:
+        pass
+
+    # Getting current day
+    current_dt = datetime.datetime.now().replace(
+        tzinfo=datetime.timezone.utc
+    )
+    curent_week_day = current_dt.strftime("%A").lower()
+
+    # Getting next friday of the week
+    friday_rl = relativedelta.relativedelta(days=1, weekday=relativedelta.FR)
+    next_friday = current_dt + friday_rl
+
+    # Every friday of the current week at 18
+    # This is will be the countdown target
+    target_date = next_friday.strftime("%Y%m%dT18")
+
+    user = update.message.from_user.first_name # noqa
+
+    msg = MESSAGES[curent_week_day]
+    if curent_week_day == 'saturday' or curent_week_day == 'sunday':
+        # If sunday or Saturday just return a single message
+        # Enjoy the weekend and leave me alone
+        update.message.reply_text(msg)
         return
 
-    if "@" in args[0]:
-        to = args[0]
-        update.message.reply_text(to)
-    text = "%20".join(args)
-    user = update.message.from_user.first_name
-    message = f"{user} faltam x dias pra felicidade"
-    update.message.reply_text(message)
+    if curent_week_day == 'friday' and current_dt.hour >= 18:
+        msg = 'Sextou caraiii!'
+        update.message.reply_text(msg)
+        return
+
+    # Any other day return the countdown
+    url = f'{BASE_URL}?iso={target_date}&p0=78&font=cursive&csz=1&msg={urllib.parse.quote(msg)}' # noqa
+    update.message.reply_text(url)
 
 
 @bot_handler
@@ -24,4 +65,4 @@ def sexout_factory():
     """
     /sextou? - countdown to friday
     """
-    return CommandHandler("sextou?", cmd_countdown, pass_args=True)
+    return CommandHandler("sextou", cmd_countdown, pass_args=True)
